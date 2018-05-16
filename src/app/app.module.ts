fe2ee06f17252cbe351e3844c1ada954b0ac540f
charlessabenin <charlessabenin@gmail.com>
2018-05-16 14:37:16 +0000

    Hoek updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, Injectable, Injector } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -10,7 +10,7 @@ import { Toast } from '@ionic-native/toast';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Camera } from '@ionic-native/camera';
 import { File } from '@ionic-native/file';
-//import { Pro } from '@ionic/pro';
+import { Pro } from '@ionic/pro';
 import { FileTransfer } from '@ionic-native/file-transfer';
 import { BackgroundMode } from '@ionic-native/background-mode';
 
@@ -44,6 +44,36 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
 //const IonicPro = Pro.init('c05474bf', {
  // appVersion: "0.0.1"
 //});
+
+Pro.init('c05474bf', {
+  appVersion: '0.0.2'
+})
+
+Pro.deploy.info().then((info) => {
+  console.log(info);
+})
+
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch(e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
 
 
 @NgModule({
